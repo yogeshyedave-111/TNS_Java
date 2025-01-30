1220b85b63844c83a92e19399ae8dbd938800db9
yogeshyedave-111 <yogeshyedave268@gmail.com>
2025-01-30 15:55:33 +0530

    On Day 2

---
+++ Day_1/src/com/Day_2/D2.java
@@ -0,0 +1,33 @@
+package com.Day_2;
+
+public class D2 {
+
+	public static void main(String[] args) {
+		byte b=10;
+		int i=b;
+		System.out.println(i);
+
+		
+		float f=100/3f;
+		double d=f;
+		System.out.println(f);
+		
+		char ch='A';
+		
+		int i1=ch;
+		
+		System.out.println(i1);
+
+		double f1=10.521f;
+		long f11=(long) f1;
+		long abc=9876543210l;
+		int i12=(int) abc;
+		
+		System.out.println(i12);
+		
+		int xyz=130;
+		byte qwe=(byte)xyz;
+		
+		System.out.println(qwe);
+	}
+}
