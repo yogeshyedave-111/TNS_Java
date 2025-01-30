a1aaec0d2619183e40ab1b06801639f90ff1621a
lenovo <lenovo@192.168.0.103>
2025-01-30 13:36:51 +0530

    Yogesh

---
+++ Day_1/src/com/Day_1/D1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.Day_1;
+
+/**
+ * 
+ */
+public class D1 {
+
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println("asdfsag");
+
+	}
+
+}
